25b3bc9ad73cefbc95c8d3b7334b5e6e1c3f94c9
Alexander Raúl Cabrera Guevara <arcabrerag@outlook.com>
2014-11-15 10:21:38 -0500

    PiensaBinario.Vs.1.0
    Completado en tiempo de ocio. :japanese_goblin:

---
+++ PiensaBinario/PensandoBinario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiensaBinario
+{
+    public class PensandoBinario
+    {
+        public int GetNumeroDeCombinaciones(int numero)
+        {
+            return Convert.ToInt32(Math.Pow(2,numero));
+        }
+    }
+}
+
+
+
---
+++ TesteaBinario/BinarioTesting.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using PiensaBinario;
+
+namespace TesteaBinario
+{
+    [TestFixture]
+    public class BinarioTesting
+    {
+
+        private PensandoBinario pensandoBinario { get; set; }
+
+        [SetUp]
+        public void InstaciasAntesDeCadaTest()
+        {
+            // Arrange
+            pensandoBinario = new PensandoBinario();
+        }
+
+        [Test]
+        public void CombinacionBinarios3y8()
+        {
+            // Act
+            var result = pensandoBinario.GetNumeroDeCombinaciones(3);
+
+            // Assert
+            Assert.AreEqual(8, result);
+        }
+
+
+        [Test]
+        public void CombinacionBinarios2y4()
+        {
+            // Act
+            var result = pensandoBinario.GetNumeroDeCombinaciones(2);
+
+            // Assert
+            Assert.AreEqual(4, result);
+        }
+
+
+        [Test]
+        public void CombinacionBinarios4y16()
+        {
+            // Act
+            var result = pensandoBinario.GetNumeroDeCombinaciones(4);
+
+            // Assert
+            Assert.AreEqual(16, result);
+        }
+
+
+
+    }
+}
